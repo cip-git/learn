8e7df8d244b7569480e265e056c37f89493d879e
Ciprian Tanase <ciprian.dorin.tanase@ibm.com>
2020-10-27 09:19:04 +0200

    27.10.2020

--- learn.java/src/main/java/oc/p/_11/_4/functionalProgramming/workingWithAdvancedStreamPipelineConcepts/collectingResults/methods/groupingBy/R.java
+++ learn.java/src/main/java/oc/p/_11/_4/functionalProgramming/workingWithAdvancedStreamPipelineConcepts/collectingResults/methods/groupingBy/R.java
@@ -1,13 +1,8 @@
 package oc.p._11._4.functionalProgramming.workingWithAdvancedStreamPipelineConcepts.collectingResults.methods.groupingBy;
 
+import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toSet;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 
 import utils.print.Print;
@@ -15,43 +10,27 @@ import utils.resources.src.Src;
 
 class R {
 
-    static void m(){
-        final Map<Boolean, List<Path>> collect = Src.Main.JAVA
-                .get()
-                .collect(groupingBy(Files::isDirectory));
-
-        collect
-                .get(true)
-                .forEach(System.out::println);
-    }
-
-    static void m2(){
-        final Map<Boolean, Map<Integer, Map<Boolean, List<Path>>>> a = Src.Main.JAVA
+    static void m() {
+        final TreeMap<String, TreeMap<Integer, TreeMap<Boolean, Long>>> collect = Src.Main.JAVA
                 .get()
                 .collect(
                         groupingBy(
-                                Files::isDirectory,
+                                p -> p.getFileName().toString(),
+                                TreeMap::new,
                                 groupingBy(
                                         p -> p.getNameCount(),
-                                        groupingBy(p -> p.getFileName().toString().startsWith("a")))));
+                                        TreeMap::new,
+                                        groupingBy(
+                                                p -> p.isAbsolute(),
+                                                TreeMap::new,
+                                                counting()))
+                        ));
 
-        Print.print(a.get(true).get(7));
-    }
-
-    static void m3(){
-        final TreeMap<Integer, Set<Path>> collect = Src.Main.JAVA
-                .get()
-                .collect(
-                        groupingBy(
-                                p -> p.getNameCount(),
-                                TreeMap::new,
-                                toSet()));
         Print.print(collect);
     }
 
     public static void main(String[] args) {
-//        m();
-//        m2();
-        m3();
+        m();
     }
+
 }
