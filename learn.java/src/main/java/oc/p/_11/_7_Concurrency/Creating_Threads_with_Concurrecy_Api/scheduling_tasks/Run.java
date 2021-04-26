e3750c66c9647570418da7342ccb1b49725320e5
Ciprian Tanase <ciprian.dorin.tanase@ibm.com>
2021-04-27 00:00:44 +0300

    OCP

---
+++ learn.java/src/main/java/oc/p/_11/_7_Concurrency/Creating_Threads_with_Concurrecy_Api/scheduling_tasks/Run.java
@@ -0,0 +1,4 @@
+package oc.p._11._7_Concurrency.Creating_Threads_with_Concurrecy_Api.scheduling_tasks;
+
+class Run {
+}
---
+++ learn.java/src/main/java/oc/p/_11/_7_Concurrency/Creating_Threads_with_Concurrecy_Api/waiting_for_all_tasks_to_finish/Run.java
@@ -0,0 +1,4 @@
+package oc.p._11._7_Concurrency.Creating_Threads_with_Concurrecy_Api.waiting_for_all_tasks_to_finish;
+
+class Run {
+}
