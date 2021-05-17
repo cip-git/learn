111aa182976a37423372294599e619ab43a2757e
Ciprian Tanase <ciprian.dorin.tanase@ibm.com>
2021-05-17 11:07:53 +0300

    14.05.2021

---
+++ learn.java/src/main/java/oc/p/_11/qs/enthuware/foundation_test/_1/_37/R.java
@@ -0,0 +1,6 @@
+package oc.p._11.qs.enthuware.foundation_test._1._37;
+
+class R {
+
+    protected static Runnable r = () -> {};
+}
---
+++ learn.java/src/main/java/oc/p/_11/qs/enthuware/foundation_test/_1/_5/R.java
@@ -0,0 +1,22 @@
+package oc.p._11.qs.enthuware.foundation_test._1._5;
+
+class R {
+
+    static void m(){
+        var i = 4;
+
+        switch (i){
+        }
+    }
+
+    static void m2(){
+        var i = 5;
+
+        switch (i){
+            default:
+                System.out.println("i");
+            case 2:
+                break;
+        }
+    }
+}
